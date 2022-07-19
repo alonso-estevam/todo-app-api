432c6dc7f0b17c45803f67fc14d35bbf2a2253a8
alonso-estevam <alonso_estevam@outlook.com>
2022-07-18 22:08:15 -0300

    del

--- src/importando.js
+++
@@ -1,8 +0,0 @@
-// import {soma,obj,string} from "./exportando"
-
-// ou com o nome do arquivo
-import exportando from "./exportando.js"
-
-console.log(exportando.soma(1,2));
-console.log(exportando.obj);
-console.log(exportando.string);
