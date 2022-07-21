a9e9f9749712bbe07039aa5f93fbe1ab48861ca8
alonso-estevam <alonso_estevam@outlook.com>
2022-07-21 10:41:26 -0300

    feat: add bd file

---
+++ src/infra/bd.js
@@ -0,0 +1,19 @@
+// Nesse arquivo, vamos exportar um objeto contendo um campo para usuários e outro para tarefas
+
+// Criando uma "tabela" usuario
+const usuario = []
+
+// Criando uma "tabela" tarefa
+const tarefa = []
+
+// Exportando o "banco de dados"
+export default{usuario, tarefa}
+
+/*
+    Ou assim:
+    export default {
+    "usuario":[],
+    "tarefa":[]
+    }
+
+*/
