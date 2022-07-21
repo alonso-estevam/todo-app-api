df99c1a5476d78944d3e625c90ece4893b4af0c5
alonso-estevam <alonso_estevam@outlook.com>
2022-07-21 10:54:40 -0300

    feat: add password validation

--- src/models/usuario-model.js
+++ src/models/usuario-model.js
@@ -1,8 +1,33 @@
+import bd from '../infra/bd.js'
+
+let id = 0
+
 class Usuario {
     constructor(nome, email, senha) {
+        this.id = id++;
         this.nome = nome;
         this.email = email;
-        this.senha = senha
+        // se for passado alguma senha, chama a função de validação
+        this.senha = senha ? this.validaSenha(senha) : undefined
+    }
+
+     // metodo de validacao de senha
+     validaSenha = (senha)=>{
+        if(senha.length >= 6){
+            return senha
+        }else{
+            throw new Error("Senha com tamanho errado!")
+        }
+    }
+
+    // método para inserir usuário no banco de dados
+    insereUsuario = (usuario)=>{
+        bd.usuario.push(usuario)
+    }
+
+    // método para pegar todos os usuários do banco de dados
+    pegaUsuarios = ()=>{
+        return bd.usuario
     }
 }
 
