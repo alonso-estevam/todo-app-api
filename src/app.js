be6c13a05472324a5a14f339c81ec0d43250bc14
alonso-estevam <alonso_estevam@outlook.com>
2022-07-21 10:35:11 -0300

    feat: import middlewares

--- src/app.js
+++ src/app.js
@@ -5,13 +5,25 @@ import express from 'express'
 import usuarioController from './controllers/usuario-controller.js'
 import tarefaController from './controllers/tarefa-controller.js'
 
+// import de middlewares
+import autenticacao from './middleware/autenticacao.js'
+
+// importando o bd (VERIFICAR)
+import bd from './infra/bd.js'
+
 // instanciando o servidor
 const app = express()
 // escolhendo a porta em que o servidor será aberto
 const port = 3000
-app.use(express.json());
 
-// chamando os controllers e passando o servidor como parametro
+// middlewares
+app.use(express.json()) // middleware que faz o parse do json do body
+
+// outros middlewares (validações, autenticações, tratamento de erros)
+// usado se necessário dependendo das regras de negócio do processo
+autenticacao(app)
+
+// chamando os controllers e passando o servidor como parâmetro
 usuarioController(app)
 tarefaController(app)
 
